4cb427317c90f2c2541b1edafd511fcc2c52b4a3
Fracisco Chacon <Fracisco Chacon@DESKTOP-6G7DPU6>
2024-05-25 22:13:16 -0600

    Vista de libros

--- Sistema_BookHub_Online/Controllers/HomeController.cs
+++ Sistema_BookHub_Online/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Sistema_BookHub_Online.Models;
 using System.Diagnostics;
 
@@ -7,14 +8,27 @@ namespace Sistema_BookHub_Online.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly libreriaContext _context;
+        public HomeController(libreriaContext context, ILogger<HomeController> logger)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            var listaIndex = (from l in _context.libros
+                              join pr in _context.prestamos on l.id_Libros equals pr.id_libro
+                              join su in _context.sucursales on pr.id_sucursal equals su.idsucursales
+                              select new
+                              {
+                                  IdLibros = l.id_Libros,
+                                  Titulo = l.Titulo,
+                                  Ejemplares = l.ejemplares,
+                                  Genero = l.Genero,
+                                  NombreSucursal = su.Nombre
+                              }).ToList();
+            ViewData["listaIndex"] = listaIndex;
             return View();
         }
 
