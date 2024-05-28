6a2485303943589faefd8f93bcd0211815638faa
Fernando Herrera <u.gomezherrera@gmail.com>
2024-05-28 16:01:17 -0600

    Campos default en modelo: prestamos

--- Sistema_BookHub_Online/Models/prestamos.cs
+++ Sistema_BookHub_Online/Models/prestamos.cs
@@ -7,8 +7,8 @@ namespace Sistema_BookHub_Online.Models
         [Key]
         public int id_libro { get; set; }
         public int id_usuario { get; set; }
-        public DateTime Fecha_prestamo { get; set; }
-        public string? Estado { get; set; }
+        public DateTime Fecha_prestamo { get; set; } = DateTime.Now;
+        public string? Estado { get; set; } = "SI";
         public int id_sucursal { get; set;}
 
     }
